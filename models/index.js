ccc97d2e75061bb830c32e12728236b334e0a412
MuhamadAkmalAmran <akmal.amran52@gmail.com>
2023-12-04 21:51:50 +0700

    resolve models/index.js

--- models/index.js
+++ models/index.js
@@ -1,44 +1,43 @@
-const dbConfig = require ('../config/config');
-const {Sequelize , DataTypes} = require('sequelize');
+'use strict';
 
-const sequelize = new Sequelize(
-      dbConfig.Database,
-      dbConfig.Username,
-      dbConfig.Password,{
-            host: dbConfig.Host,
-            dialect : dbConfig.dialect,
-            operatorsAliase : false,
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const process = require('process');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.js')[env];
+const db = {};
 
-      pool  :{
-            max : dbConfig.max,
-            min : dbConfig.min,
-            acquire : dbConfig.pool.acquire,
-            idle : dbConfig.pool.idle
-      }
-      }
-)
-sequelize.authenticate()
-.then(()=>{
-      console.log('Terkoneksi')
-})
-.catch(err => {
-      console.log('Error' + err)
-})
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
-const db = {}
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (
+      file.indexOf('.') !== 0 &&
+      file !== basename &&
+      file.slice(-3) === '.js' &&
+      file.indexOf('.test.js') === -1
+    );
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
 
-db.Sequelize = Sequelize
-db.sequelize = sequelize
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
 
-
-db.datasets = require('./datasets')(sequelize, DataTypes)
-
-
-
-
-db.sequelize.sync({force:false})
-.then(()=>{
-      console.log('yes re-sync done!')
-})
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
 module.exports = db;
