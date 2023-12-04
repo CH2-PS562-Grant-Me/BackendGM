a58ddbe1efdabeb98e2714b96e5a78ec7d9a90e8
MuhamadAkmalAmran <akmal.amran52@gmail.com>
2023-12-04 21:52:19 +0700

    resolve models/profile.js

---
+++ models/profile.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Profile extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Profile.init({
+    nama: DataTypes.STRING,
+    no_telpon: DataTypes.STRING,
+    foto_profile: DataTypes.STRING,
+    jenis_kelamin: DataTypes.STRING,
+    tgl_lahir: DataTypes.DATE,
+    transkip: DataTypes.STRING,
+    ktp: DataTypes.STRING,
+    ijazah: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Profile',
+  });
+  return Profile;
+};
